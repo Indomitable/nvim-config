27fcf21593f012b8f02bd244991cd8213ff7dc77
vmladenov <v.mladenov@hotmail.com>
2024-05-11 00:06:57 +0200

    shortuts update

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -23,7 +23,7 @@ return {
       end
     })
 
-    vim.keymap.set("n", "<leader>l", function ()
+    vim.keymap.set("n", "<leader>lf", function ()
       lint.try_lint();
     end, { desc = "Lint file" })
   end
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -85,16 +85,20 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+		vim.keymap.set("n", "<leader>l", builtin.find_files, { desc = "[S]earch [F]iles" })
+    --vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+		vim.keymap.set("n", "<leader>sp", builtin.live_grep, { desc = "[S]earch [P]attern" })
 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 		vim.keymap.set("n", "<leader>su", "<cmd>Telescope undo<cr>", { desc = "[S]earch [U]ndo history" })
 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+		vim.keymap.set("n", "<leader>sgc", builtin.git_commits, { desc = "[S]earch [G]it [C]ommits" })
+		vim.keymap.set("n", "<leader>sgb", builtin.git_branches, { desc = "[S]earch [G]it [B]ranches" })
+		vim.keymap.set("n", "<leader>sgs", builtin.git_stash, { desc = "[S]earch [G]it [S]tash" })
 
-		-- Slightly advanced example of overriding default behavior and theme
+    -- Slightly advanced example of overriding default behavior and theme
 		vim.keymap.set("n", "<leader>/", function()
 			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
 			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
